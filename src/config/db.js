aa84ee33f948dc9def9415dba5360262ae4a5552
Kevin CT <kcayo6155@uta.edu.ec>
2024-10-05 12:07:55 -0500

    refactor

--- src/config/db.js
+++ src/config/db.js
@@ -6,12 +6,14 @@ const conectarBD = async () => {
     await mongoose.connect(process.env.DB_MONGO, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Tiempo de espera para la selección del servidor
     });
-    console.log("Base conectada");
+    console.log("Base de datos conectada con éxito");
   } catch (error) {
-    console.log(error);
-    // 1 detiene la ejecucion del servidor.
-    process.exit(1);
+    console.error("Error al conectar a la base de datos:", error.message);
+    // Manejo alternativo en lugar de detener el proceso
+    // Podrías lanzar un error o establecer un estado en tu aplicación
+    // throw new Error("No se pudo conectar a la base de datos");
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -7,28 +7,39 @@ const encuestaRuta = require("./rutas/encuestaRuta");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Conectar a la base de datos
-conectarBD();
+// Conectar a la base de datos y luego iniciar el servidor
+const iniciarServidor = async () => {
+  try {
+    await conectarBD(); // Esperar a que la conexión se establezca
+    console.log("Base de datos conectada");
 
-// Permitir todas las solicitudes CORS desde cualquier origen
-app.use(
-  cors({
-    origin: "*", // Permite todas las solicitudes desde cualquier dominio
-  })
-);
+    // Permitir todas las solicitudes CORS desde cualquier origen
+    app.use(
+      cors({
+        origin: "*", // Permite todas las solicitudes desde cualquier dominio
+      })
+    );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false })); // Corrección aquí
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
 
-// Llamar a las APIs
-app.use("/api/encuesta", encuestaRuta);
+    // Llamar a las APIs
+    app.use("/api/encuesta", encuestaRuta);
 
-// Definir ruta inicial
-app.get("/", (req, res) => {
-  res.send("Servidor activo");
-});
+    // Definir ruta inicial
+    app.get("/", (req, res) => {
+      res.send("Servidor activo");
+    });
 
-// Definir puerto de escucha
-app.listen(port, () => {
-  console.log(`Servidor ejecutando en el puerto ${port}`);
-});
+    // Definir puerto de escucha
+    app.listen(port, () => {
+      console.log(`Servidor ejecutando en el puerto ${port}`);
+    });
+  } catch (error) {
+    console.error("Error al conectar a la base de datos:", error);
+    // Podrías optar por finalizar el proceso o manejarlo de otra manera
+    process.exit(1);
+  }
+};
+
+iniciarServidor(); // Llamar a la función para iniciar el servidor
